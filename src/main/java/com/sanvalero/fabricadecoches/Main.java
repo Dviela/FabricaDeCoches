75d111fccfce1383cf60259d40eb5d95073b2418
Dviela <a19230@svalero.com>
2022-08-28 18:47:23 +0200

    Añadida funcionalidad para cambio de marcha manual o automáticor

--- src/main/java/com/sanvalero/fabricadecoches/Main.java
+++ src/main/java/com/sanvalero/fabricadecoches/Main.java
@@ -3,7 +3,9 @@ package com.sanvalero.fabricadecoches;
 public class Main {
     public static void main(String[] args) {
     Turismo coche1 = new Turismo("7894LKJ");
-    coche1.setClimatizador("si");
-        System.out.println(coche1.isClimatizador());
+    coche1.setClimatizador("no");
+    coche1.setAutomatico("si");
+
+        System.out.println("Turismo con " + coche1.isClimatizador() + " y " + coche1.isAutomatico());
     }
 }
