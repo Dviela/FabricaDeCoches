1683769b38e9ae121d6a7699386d4436ef9a0e91
Dviela <a19230@svalero.com>
2022-08-28 18:31:04 +0200

    Añadida funcionalidad para extra Climatizador

--- src/main/java/com/sanvalero/fabricadecoches/Main.java
+++ src/main/java/com/sanvalero/fabricadecoches/Main.java
@@ -2,6 +2,8 @@ package com.sanvalero.fabricadecoches;
 
 public class Main {
     public static void main(String[] args) {
-
+    Turismo coche1 = new Turismo("7894LKJ");
+    coche1.setClimatizador("si");
+        System.out.println(coche1.isClimatizador());
     }
 }
